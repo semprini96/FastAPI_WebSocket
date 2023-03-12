c89ef3b70b36cbc44ff180b3228ddb60614455e5
semprini96 <dmikolaj15@gmail.com>
2023-03-12 22:58:38 +0100

    Add handling of multiple WebSocket connections

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import asyncio
-from typing import Dict
-from fastapi import FastAPI, Request, WebSocket
+from typing import Dict, List
+from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
 from pydantic import BaseModel
 from fastapi.templating import Jinja2Templates
 import httpx
@@ -15,6 +15,24 @@ class APIData(BaseModel):
     status: str = 'in_progress'
 
 data = APIData()
+
+
+class ConnectionManger:
+    def __init__(self) -> None:
+        self.connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.connections.append(websocket)
+
+    async def broadcast(self, b_data: str):
+        for connection in self.connections:
+            await connection.send_text(b_data)
+
+    def remove(self, websocket: WebSocket):
+        self.connections.remove(websocket)
+
+manager = ConnectionManger()
 
 
 async def background_task():
@@ -41,27 +59,27 @@ async def schedule_periodic():
     loop.create_task(background_task())
 
 
-@app.websocket('/ws')
-async def websocket_endpoint(websocket: WebSocket):
-    print('Accepting client connection...')
-    await websocket.accept()
-    while True:
-        try:
-            print('Received ping from client...')
+@app.websocket('/ws/{client_id}')
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    print(f'Accepted connection from client with ID={client_id})')
+    await manager.connect(websocket)
+    try:
+        while True:
+            print(f'Received ping from client with ID={client_id})')
             await websocket.receive_text()
             if data.status == 'completed':
                 resp = data.fact
                 data.status = 'await'
-                await websocket.send_text(resp)
+                await manager.broadcast(resp)
             elif data.status == 'in_progress':
                 resp = 'Retrieving data, please wait...'
-                await websocket.send_text(resp)
-        except Exception as e:
-            print('ERROR: ', e)
-            break
-        print('Waiting 10 seconds for ping...')
-        await asyncio.sleep(10)
-    print('Bye')
+                await manager.broadcast(resp)
+    except WebSocketDisconnect:
+        print(f'Disconnected client with ID={client_id}')
+        manager.remove(websocket)
+    # print(f'Waiting 10 seconds for ping from client with ID={client_id}')
+    # await asyncio.sleep(10)
+    # print('Bye')
 
 
 @app.get('/')
